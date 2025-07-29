ad74f5ccefe04cd2f3c1ffaee74700d15419acb9
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 20:37:14 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -2,6 +2,21 @@ const { getDefaultConfig } = require('expo/metro-config');
 
 module.exports = (async () => {
   const config = await getDefaultConfig(__dirname);
-  config.resolver.sourceExts.push('cjs');
+
+  // Allow `.cjs` and `.svg` files (for custom icons/assets)
+  config.resolver.sourceExts = [
+    ...config.resolver.sourceExts,
+    'cjs',
+    'svg'
+  ];
+
+  // Optional: treat SVGs as react-native components
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
+  };
+
+  config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+
   return config;
 })();
