8ed743d04ccb95a96ee73d46229f04a79eca23b7
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 21:03:12 +0800

    Update metro.config.js

--- metro.config.js
+++ metro.config.js
@@ -1,7 +1,19 @@
-module.exports = function (api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: ['expo-router/babel'],
+const { getDefaultConfig } = require('expo/metro-config');
+
+module.exports = async () => {
+  const config = await getDefaultConfig(__dirname);
+
+  // Support additional file extensions
+  config.resolver.sourceExts = [...config.resolver.sourceExts, 'cjs', 'svg'];
+
+  // Treat SVGs as React components
+  config.transformer = {
+    ...config.transformer,
+    babelTransformerPath: require.resolve('react-native-svg-transformer'),
   };
+
+  // Exclude SVGs from default asset handling
+  config.resolver.assetExts = config.resolver.assetExts.filter(ext => ext !== 'svg');
+
+  return config;
 };
