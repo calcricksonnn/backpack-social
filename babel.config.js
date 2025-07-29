287babbdd52dc6f738f85edebf0e0c2aa4fa695f
calcricksonnn <calcrickson22@icloud.com>
2025-07-29 21:34:10 +0800

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -3,16 +3,13 @@ module.exports = function (api) {
   return {
     presets: [
       ['babel-preset-expo', { jsxRuntime: 'automatic' }],
-      '@react-native/babel-preset',
+      // Remove @react-native/babel-preset - it conflicts with babel-preset-expo
     ],
     plugins: [
       ['@babel/plugin-transform-class-properties', { loose: true }],
       ['@babel/plugin-transform-private-methods', { loose: true }],
       ['@babel/plugin-transform-private-property-in-object', { loose: true }],
-      ['@babel/plugin-transform-react-jsx', {
-        runtime: 'automatic',
-        importSource: 'react',
-      }],
+      // Remove the manual JSX transform plugin - babel-preset-expo handles this
     ],
   };
 };
