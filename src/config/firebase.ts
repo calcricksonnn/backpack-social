123e86b0def03ce6f09775167fb0b66a74c5df1c
calcricksonnn <calcrickson22@icloud.com>
2025-07-30 00:11:00 +0800

    Update firebase.ts

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,9 +1,14 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
-import Constants from 'expo-constants';
 
-const { firebaseConfig } = Constants.manifest!.extra!; 
-// make sure to add your config in app.json→extra
+const firebaseConfig = {
+  apiKey: 'YOUR_API_KEY',
+  authDomain: 'YOUR_DOMAIN',
+  projectId: 'YOUR_PROJECT_ID',
+  storageBucket: 'YOUR_BUCKET',
+  messagingSenderId: 'YOUR_SENDER_ID',
+  appId: 'YOUR_APP_ID',
+};
 
 const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
